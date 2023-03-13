5c861224dd454243a9a6f22461c940356b7d3646
bluewhaleyt <cocohau155@gmail.com>
2023-03-13 16:24:32 +0800

    added network connection checking on start

--- app/src/main/java/com/bluewhaleyt/chatgpt/ui/activities/BaseActivity.java
+++ app/src/main/java/com/bluewhaleyt/chatgpt/ui/activities/BaseActivity.java
@@ -5,7 +5,9 @@ import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.bluewhaleyt.common.CommonUtil;
+import com.bluewhaleyt.common.IntentUtil;
 import com.bluewhaleyt.crashdebugger.CrashDebugger;
+import com.bluewhaleyt.network.NetworkUtil;
 
 public class BaseActivity extends AppCompatActivity {
 
@@ -19,6 +21,8 @@ public class BaseActivity extends AppCompatActivity {
         CommonUtil.setNavigationBarColorWithSurface(this, CommonUtil.SURFACE_FOLLOW_WINDOW_BACKGROUND);
 
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        if (!NetworkUtil.isNetworkAvailable(this)) IntentUtil.intent(this, NoNetworkActivity.class);
     }
 
     @Override
---
+++ app/src/main/java/com/bluewhaleyt/chatgpt/ui/activities/NoNetworkActivity.java
@@ -0,0 +1,39 @@
+package com.bluewhaleyt.chatgpt.ui.activities;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.bluewhaleyt.chatgpt.R;
+import com.bluewhaleyt.chatgpt.databinding.ActivityMyFilesBinding;
+import com.bluewhaleyt.chatgpt.databinding.ActivityNoNetworkBinding;
+import com.bluewhaleyt.common.CommonUtil;
+import com.bluewhaleyt.common.IntentUtil;
+import com.bluewhaleyt.component.snackbar.SnackbarUtil;
+import com.bluewhaleyt.network.NetworkUtil;
+
+public class NoNetworkActivity extends AppCompatActivity {
+
+    private ActivityNoNetworkBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityNoNetworkBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        initialize();
+    }
+
+    private void initialize() {
+
+        CommonUtil.setStatusBarColorWithSurface(this, CommonUtil.SURFACE_FOLLOW_WINDOW_BACKGROUND);
+        CommonUtil.setToolBarColorWithSurface(this, CommonUtil.SURFACE_FOLLOW_WINDOW_BACKGROUND);
+        CommonUtil.setNavigationBarColorWithSurface(this, CommonUtil.SURFACE_FOLLOW_WINDOW_BACKGROUND);
+
+        binding.btnRefresh.setOnClickListener(v -> {
+            if (NetworkUtil.isNetworkAvailable(this)) IntentUtil.intent(this, MainActivity.class);
+            else SnackbarUtil.makeErrorSnackbar(this, "No network available");
+        });
+    }
+
+}
